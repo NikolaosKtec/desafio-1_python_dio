7b9b9dd94507c14f2a2dd01c86f54aeeec466a84
Nikolaos Ferreira Daskalakis <87676644+NikolaosKtec@users.noreply.github.com>
2024-09-24 09:12:49 -0300

    updates

--- banco_class.py
+++ banco_class.py
@@ -7,6 +7,11 @@ class Banco:
         self.limite_saques = bool(False)
         self.LIMITE_SAQUES = int(3)
         self.numero_saques = int(0)
+        self.CADASTRO_PESSOA_FISICA = set()
+        # self.CADASTRO_CONTAS = set()
+        self.users = list()
+        self.contas_c = list()
+        self.seq = int(0)
     # [d] Depositar
     def deposito(self):
         
@@ -61,10 +66,13 @@ class Banco:
 
         menu = """
 
-        [d] Depositar
-        [s] Sacar
-        [e] Extrato
-        [q] Sair
+        \t[d] Depositar
+        \t[s] Sacar
+        \t[e] Extrato
+        \t[cc] Criar conta corrente
+        \t[lc] Listas contas
+        \t[c] Cadastrar usuario
+        \t[q] Sair
         """
         while True:
 
@@ -80,3 +88,63 @@ class Banco:
                 break
             else:
                 break
+
+    def criar_usuario(self):
+
+        nome = str(input("informe o Nome"))
+        nas = str(input("informe a Data de nascimento"))
+
+        try:
+            cpf = int(input("Informe o CPF (somente números):"))
+        except ValueError:
+            print("somente números de 0-9 !")
+        except:
+            print("algo deu errado :(")
+        
+        if( cpf in self.CADASTRO_PESSOA_FISICA):
+            print("Ops, ja existe alguém cadastrado com este CPF!, saindo do menu...")
+            return
+        
+        self.CADASTRO_PESSOA_FISICA.add(cpf)
+        self.users.append(Usuario(cpf, nome, nas, "endereço qualquer"))
+        
+    def criar_conta_corrente(self):
+        
+        agencia = input("informe agência (x-xxxx)")
+
+        try:
+            cpf = int(input("informe o cpf do titular"))
+        except ValueError:
+            print("somente números de 0-9 !")
+        except:
+            print("algo deu errado :(")
+
+        if( not cpf in self.CADASTRO_PESSOA_FISICA):
+            print("Não existe este CPF cadastrado em nosso banco! , saindo do menu...")
+            return
+        
+        self.seq=+1
+        n_conta = "000"+self.seq
+
+        self.contas_c.append(Conta_c(agencia,n_conta,cpf))
+
+    def listar_contas(self):
+        print("\n================ CONTAS ================ \n")
+        for i in self.contas_c:
+            print("\t================ TITULAR ================\n")
+            print(f"{i.usuario_key}\n")
+            print("\t================ N° CONTA ================\n")
+            print(f"{i.n_conta}\n")
+
+class Usuario:
+    def __init__(self, cpf, nome, nasc, ender):
+        self.CPF = int(cpf)
+        self.NOME = str(nome)
+        self.DATA_NAS = str(nasc)
+        self.ENDERECO = str(ender)
+
+class Conta_c:
+    def __init__(self,agencia, conta, usua_cpf):
+        self.agencia = str(agencia)
+        self.n_conta = str(conta)
+        self.usuario_key = int(usua_cpf)
